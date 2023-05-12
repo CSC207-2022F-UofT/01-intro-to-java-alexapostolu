1f44be4879a12118399045affd63f39860bfd540
Alex <60247838+alexapostolu@users.noreply.github.com>
2023-05-12 15:52:35 -0400

    Update Basics.java

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -149,7 +149,7 @@ public class Basics {
         // Fill in the rest of the body here
         
         String[] s = to_split.split(" ");
-        for (int i = 0; i < s.length; ++i);
+        for (int i = 0; i < s.length; ++i)
             ret.append(s[i].charAt(0));
         return ret.toString();
     }
